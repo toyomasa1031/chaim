3cfbde09388ed1d248e7d0dae249b034b569c795
toyomasa1031 <76829611+toyomasa1031@users.noreply.github.com>
2022-09-03 14:27:45 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showIcon(IconNames.Happy)
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+})
+radio.setGroup(1)
 basic.forever(function () {
 	
 })
