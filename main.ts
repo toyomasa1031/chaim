7b0eed6d054c0094e5c61e9173fa1c058516a728
toyomasa1031 <76829611+toyomasa1031@users.noreply.github.com>
2022-10-10 16:15:44 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,6 +16,9 @@ radio.onReceivedNumber(function (receivedNumber) {
     basic.clearScreen()
 })
 input.onButtonPressed(Button.A, function () {
-	
+    radio.sendNumber(2)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(3)
 })
 radio.setGroup(1)
