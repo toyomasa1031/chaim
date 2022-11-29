5ea885a1821a64c12be93293085accba7d5a058f
toyomasa1031 <76829611+toyomasa1031@users.noreply.github.com>
2022-11-29 18:13:08 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,65 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
+        角度 = 90
+        pins.servoWritePin(AnalogPin.P0, 角度)
         basic.showLeds(`
-            . # # # .
-            # . . . #
-            # . . . #
-            # . . . #
-            . # # # .
+            # . . . .
+            # # . . .
+            # # # . .
+            # # . . .
+            # # . . .
             `)
-    } else {
-        music.playTone(392, music.beat(BeatFraction.Whole))
-        basic.showIcon(IconNames.No)
+        角度 = 0
+        pins.servoWritePin(AnalogPin.P0, 角度)
     }
-    basic.pause(100)
+    if (receivedNumber == 1) {
+        角度 = 90
+        pins.servoWritePin(AnalogPin.P2, 角度)
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . # # #
+            . . . # #
+            . . . # #
+            `)
+        角度 = 0
+        pins.servoWritePin(AnalogPin.P2, 角度)
+    }
     basic.clearScreen()
 })
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(2)
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+    basic.pause(100)
+    for (let index = 0; index < 11; index++) {
+        strip.rotate(1)
+        strip.show()
+        basic.pause(200)
+    }
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 3; index++) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.pause(200)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        basic.pause(200)
+        strip.showRainbow(1, 360)
+        basic.pause(200)
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(3)
+    strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Green))
+    strip.show()
+    basic.pause(100)
+    for (let index = 0; index < 11; index++) {
+        strip.rotate(-1)
+        strip.show()
+        basic.pause(200)
+    }
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
 })
-radio.setGroup(1)
+let 角度 = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P1, 12, NeoPixelMode.RGB)
